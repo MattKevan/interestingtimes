f48d866fc0dc65a021eee276e171f2478daf31a5
Matt <matt@GBBAL13096.broadband>
2020-05-31 14:50:13 +0100

    Delete

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
 $(document).ready(function() {
-    $('.item').matchHeight();
-
-   $('.related-producdts-pager').rpmPagination({ 
-        domElement: '.related-prodducts-paged',
-        limit: 9
-        //refresh: true
-
-    });
+    
     $('.listing-pvager').rpmPagination({ 
         domElement: '.listing-pvaged',
         limit: 12
